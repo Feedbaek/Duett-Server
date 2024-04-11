30d7530c400e199c666eb05a8a97bc70b14a26ca
Feedbaek <nicek789@naver.com>
2024-04-11 16:31:14 +0900

    Init

---
+++ src/main/java/Dino/MyMatching/MyMatchingApplication.java
@@ -0,0 +1,13 @@
+package Dino.MyMatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Dino/MyMatching/MyMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package Dino.MyMatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
