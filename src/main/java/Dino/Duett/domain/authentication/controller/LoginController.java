4791dbd70d3a5d0b924efc0ad36ecb148bb55de3
Feedbaek <nicek789@naver.com>
2024-06-12 14:29:35 +0900

    Docs: login API Swagger 수정

--- src/main/java/Dino/Duett/domain/authentication/controller/LoginController.java
+++ src/main/java/Dino/Duett/domain/authentication/controller/LoginController.java
@@ -5,6 +5,8 @@ import Dino.Duett.global.dto.JsonBody;
 import Dino.Duett.global.dto.TokenDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class LoginController {
     @Operation(summary = "로그인")
-    @PostMapping(value = "")
-    public JsonBody<TokenDto> login(LoginDto loginDto) {
+    @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public JsonBody<TokenDto> login(@Valid LoginDto loginDto) {
         // 실제 구현은 security filter 에서 처리. 여기는 API 명세하기 위한 용도
         loginDto.setPhoneNumber("01012345678");
         loginDto.setVerificationCode("123456");
