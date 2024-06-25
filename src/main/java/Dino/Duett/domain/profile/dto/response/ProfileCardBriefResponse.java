f0eccbe6e4d3e0f2c858ebd4f9cbd79e2616e0c3
sor999 <sorsor999@naver.com>
2024-06-25 16:30:03 +0900

    Feat: 프로필 카드 API 구현

--- src/main/java/Dino/Duett/domain/profile/dto/response/ProfileCardBriefResponse.java
+++ src/main/java/Dino/Duett/domain/profile/dto/response/ProfileCardBriefResponse.java
@@ -1,19 +1,18 @@
 package Dino.Duett.domain.profile.dto.response;
 
+import lombok.Getter;
 import Dino.Duett.domain.music.dto.response.MusicResponse;
 import Dino.Duett.domain.profile.enums.MbtiType;
-import Dino.Duett.domain.tag.dto.response.TagResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
-import lombok.Getter;
 
 import java.util.List;
 
-@Schema(description = "프로필 카드 조회 요약 응답")
+@Schema(description = "프로필 간략 응답")
 @Getter
 @Builder
-public class ProfileCardSummaryResponse {
-    @Schema(description = "프로필 ID",example = "1")
+public class ProfileCardBriefResponse {
+    @Schema(description = "프로필 ID", example = "1")
     Long profileId;
     @Schema(description = "사용자의 이름", example = "name")
     String name;
@@ -21,12 +20,9 @@ public class ProfileCardSummaryResponse {
     String birthDate;
     @Schema(description = "MBTI 유형", example = "ENTP")
     MbtiType mbti;
-    @Schema(description = "한 줄 소개", example = "crush")
-    String oneLineIntroduction;
-    @Schema(description = "사용자와의 거리", example = "1.5")
-    double distance;
-    @Schema(description = "인생곡 리스트", example = "[{\"title\": \"title\", \"artist\": \"artist\", \"url\": \"url\"}]")
-    List<MusicResponse> lifeMusics;
+    @Schema(description = "인생곡", example = "[{\"title\": \"title\", \"artist\": \"artist\", \"url\": \"url\"}]")
+    MusicResponse lifeMusic;
     @Schema(description = "태그", example = "[\"팝\", \"콘서트\"]")
     List<String> tags;
 }
+
--- src/main/java/Dino/Duett/domain/profile/service/ProfileService.java
+++ src/main/java/Dino/Duett/domain/profile/service/ProfileService.java
@@ -13,23 +13,26 @@ import Dino.Duett.domain.music.service.MusicService;
 import Dino.Duett.domain.profile.dto.request.ProfileInfoRequest;
 import Dino.Duett.domain.profile.dto.request.ProfileIntroRequest;
 import Dino.Duett.domain.profile.dto.request.ProfileMusicRequest;
+import Dino.Duett.domain.profile.dto.response.ProfileHomeResponse;
 import Dino.Duett.domain.profile.dto.response.ProfileInfoResponse;
 import Dino.Duett.domain.profile.dto.response.ProfileIntroResponse;
 import Dino.Duett.domain.profile.dto.response.ProfileMusicResponse;
 import Dino.Duett.domain.profile.entity.Profile;
 import Dino.Duett.domain.profile.exception.ProfileException;
-import Dino.Duett.domain.profile.repository.ProfileRepository;
 import Dino.Duett.domain.tag.dto.response.TagByTypeResponse;
 import Dino.Duett.domain.tag.enums.TagType;
 import Dino.Duett.domain.tag.service.ProfileTagService;
-import Dino.Duett.global.util.Validator;
+import Dino.Duett.global.utils.Validator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import static Dino.Duett.global.enums.LimitConstants.MUSIC_MAX_LIMIT;
+
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProfileService {
 
     private final MemberRepository memberRepository;
@@ -37,44 +40,76 @@ public class ProfileService {
     private final MoodService moodService;
     private final ImageService imageService;
     private final ProfileTagService profileTagService;
-    private final ProfileRepository profileRepository;
-//    /**
-//     * 마이페이지 프로필 조회
-//     * @param memberId 사용자 id
-//     * @return ProfileResponse
-//     */
-//    @Transactional
-//    public ProfileResponse getProfile(final Long memberId) {
-//        Member member = memberRepository.findById(memberId).orElseThrow(MemberException.MemberNotFoundException::new);
-//        Profile profile = validateProfileIsNull(member.getProfile());
-//        return ProfileResponse.builder()
-//                .profileId(profile.getId())
-//                .name(profile.getName())
-//                .birthDate(profile.getBirthDate())
-//                .mbti(profile.getMbti())
-//                .oneLineIntroduction(profile.getOneLineIntroduction())
-//                .profileImageUrl(imageService.getUrl(profile.getProfileImage()))
-//                .musicTags(profileTagService.getProfileTags(profile.getId(), TagType.MUSIC))
-//                .hobbyTags(profileTagService.getProfileTags(profile.getId(), TagType.HOBBY))
-//                .lifeMusics(musicService.getMusics(profile.getId()))
-//                .mood(MoodResponse.of(
-//                        profile.getMood().getTitle(),
-//                        profile.getMood().getArtist(),
-//                        imageService.getUrl(profile.getMood().getMoodImage())))
-//                .selfIntroduction(profile.getSelfIntroduction())
-//                .likeableMusicTaste(profile.getLikeableMusicTaste())
-//                .build();
-//    }
 
-    @Transactional
+    public ProfileHomeResponse getProfileHome(final Long memberId){
+        Member member = memberRepository.findById(memberId).orElseThrow(MemberException.MemberNotFoundException::new);
+        Profile profile = validateProfileIsNull(member.getProfile());
+
+        return ProfileHomeResponse.builder()
+                .profileImageUrl(profile.getProfileImage() != null ? imageService.getUrl(profile.getProfileImage()) : null)
+                .name(profile.getName())
+                .birthDate(profile.getBirthDate())
+                .mbti(profile.getMbti())
+                .infoCount(getProfileInfoCount(profile))
+                .introCount(getProfileIntroCount(profile))
+                .musicCount(getProfileMusicCount(profile))
+                .build();
+    }
+
+    private int getProfileInfoCount(final Profile profile){
+        int count = 0;
+        if(profile.getName() != null){
+            count++;
+        }
+        if(profile.getOneLineIntroduction() != null){
+            count++;
+        }
+        return count;
+    }
+
+    private int getProfileIntroCount(final Profile profile){
+        int count = 0;
+        if(profile.getMbti() != null){
+            count++;
+        }
+        if(profile.getProfileTags() != null){
+            if(profileTagService.checkFeaturedProfileTagsCount(profile)){
+                count++;
+            }
+        }
+        if(profile.getSelfIntroduction() != null){
+            count++;
+        }
+        if(profile.getLikeableMusicTaste() != null){
+            count++;
+        }
+        return count;
+    }
+
+    private int getProfileMusicCount(final Profile profile){
+        int count = 0;
+        if(profile.getMusics() != null){
+            if(profile.getMusics().size() == MUSIC_MAX_LIMIT.getLimit())
+                count++;
+        }
+        Mood mood = profile.getMood();
+        if(mood != null){
+            count++;
+        }
+        return count;
+    }
+
     public ProfileInfoResponse getProfileInfo(final Long memberId){
         Member member = memberRepository.findById(memberId).orElseThrow(MemberException.MemberNotFoundException::new);
         Profile profile = validateProfileIsNull(member.getProfile());
 
-        return ProfileInfoResponse.of(
-                profile,
-                profile.getProfileImage() != null ? imageService.getUrl(profile.getProfileImage()) : null
-        );
+        return ProfileInfoResponse.builder()
+                .profileImageUrl(profile.getProfileImage() != null ? imageService.getUrl(profile.getProfileImage()) : null)
+                .name(profile.getName())
+                .birthDate(profile.getBirthDate())
+                .gender(profile.getGender().getValue())
+                .oneLineIntroduction(profile.getOneLineIntroduction())
+                .build();
     }
 
     @Transactional
@@ -100,19 +135,17 @@ public class ProfileService {
         );
     }
 
-    @Transactional
     public ProfileIntroResponse getProfileIntro(final Long memberId){
         Member member = memberRepository.findById(memberId).orElseThrow(MemberException.MemberNotFoundException::new);
         Profile profile = validateProfileIsNull(member.getProfile());
 
-        return ProfileIntroResponse.of(
-                profile.getMbti(),
-                profileTagService.getProfileTags(profile.getId(), TagType.MUSIC),
-                profileTagService.getProfileTags(profile.getId(), TagType.HOBBY),
-                profile.getSelfIntroduction(),
-                profile.getLikeableMusicTaste()
-
-        );
+        return ProfileIntroResponse.builder()
+                .mbti(profile.getMbti())
+                .musicTags(profileTagService.getProfileTags(profile.getId(), TagType.MUSIC))
+                .hobbyTags(profileTagService.getProfileTags(profile.getId(), TagType.HOBBY))
+                .selfIntroduction(profile.getSelfIntroduction())
+                .likeableMusicTaste(profile.getLikeableMusicTaste())
+                .build();
     }
 
     @Transactional
@@ -129,8 +162,6 @@ public class ProfileService {
         profileTagService.changeProfileTags(memberId, profileIntroRequest.getMusicTags(), profileIntroRequest.getHobbyTags());
     }
 
-
-    @Transactional
     public ProfileMusicResponse getProfileMusic(final Long memberId) {
         Member member = memberRepository.findById(memberId).orElseThrow(MemberException.MemberNotFoundException::new);
         Profile profile = validateProfileIsNull(member.getProfile());
@@ -143,7 +174,7 @@ public class ProfileService {
     }
 
     @Transactional
-    public ProfileMusicResponse updateProfileMusic(final Long memberId, final ProfileMusicRequest profileMusicRequest){
+    public void updateProfileMusic(final Long memberId, final ProfileMusicRequest profileMusicRequest){
         Member member = memberRepository.findById(memberId).orElseThrow(MemberException.MemberNotFoundException::new);
         Profile profile = validateProfileIsNull(member.getProfile());
 
@@ -159,7 +190,7 @@ public class ProfileService {
                     profileMusicRequest.getMood());
         }
 
-        return ProfileMusicResponse.of(
+        ProfileMusicResponse.of(
                 MusicResponse.of(
                         profile.getMusics()),
                 MoodResponse.of(
@@ -177,7 +208,6 @@ public class ProfileService {
         return profile;
     }
 
-    @Transactional
     public TagByTypeResponse getProfileTagsWithAllTagsByMemberId(final Long memberId){
         Member member = memberRepository.findById(memberId).orElseThrow(MemberException.MemberNotFoundException::new);
         Profile profile = validateProfileIsNull(member.getProfile());
