412712d0bd79e1899030321b094b5081f710e933
sor999 <sorsor999@naver.com>
2024-06-26 16:29:00 +0900

    Feat: 멤버 테스트를 위한 API

--- src/main/java/Dino/Duett/domain/profile/controller/ProfileUnlockController.java
+++ src/main/java/Dino/Duett/domain/profile/controller/ProfileUnlockController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class ProfileUnlockController {
     private final ProfileUnlockService profileUnlockService;
 
