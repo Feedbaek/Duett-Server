37d22150ca97738bedca2b7e821b93052287c12b
ddr4869 <bikdu@naver.com>
2024-07-25 10:45:38 +0900

    [hotfix] TermInitializer ApplicationRunner 롤백

--- src/main/java/Dino/Duett/domain/term/TermInitializer.java
+++ src/main/java/Dino/Duett/domain/term/TermInitializer.java
@@ -14,31 +14,31 @@ import java.nio.file.Path;
 
 @Component
 public class TermInitializer implements ApplicationRunner {
-    private final TermRepository termRepository;
-    private final TermService termService;
-
-    public TermInitializer(TermRepository termRepository, TermService termService) {
-        this.termRepository = termRepository;
-        this.termService = termService;
-    }
-
+//    private final TermRepository termRepository;
+//    private final TermService termService;
+//
+//    public TermInitializer(TermRepository termRepository, TermService termService) {
+//        this.termRepository = termRepository;
+//        this.termService = termService;
+//    }
+//
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        if (termRepository.count() == 0) {
-            Term signUpTerms = Term.builder()
-                    //.content("Initial Sign-Up Terms Content")
-                    .content(termService.readFileContent("signup-policy.txt"))
-                    .version(1L)
-                    .type(TermType.SIGN_UP)
-                    .build();
-            termRepository.save(signUpTerms);
-
-            Term privacyPolicy = Term.builder()
-                    .content(termService.readFileContent("privacy-policy.txt"))
-                    .version(1L)
-                    .type(TermType.PRIVACY_POLICY)
-                    .build();
-            termRepository.save(privacyPolicy);
-        }
+//        if (termRepository.count() == 0) {
+//            Term signUpTerms = Term.builder()
+//                    //.content("Initial Sign-Up Terms Content")
+//                    .content(termService.readFileContent("signup-policy.txt"))
+//                    .version(1L)
+//                    .type(TermType.SIGN_UP)
+//                    .build();
+//            termRepository.save(signUpTerms);
+//
+//            Term privacyPolicy = Term.builder()
+//                    .content(termService.readFileContent("privacy-policy.txt"))
+//                    .version(1L)
+//                    .type(TermType.PRIVACY_POLICY)
+//                    .build();
+//            termRepository.save(privacyPolicy);
+//        }
     }
 }
